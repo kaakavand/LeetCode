937e56ae5e791aeabb6a1484007fa6b4656819a3
Amirhosein Kakavand <94612720+kaakavand@users.noreply.github.com>
2024-03-17 23:17:45 +0330

    Update roman-to-integer.ts convert to switch case

--- roman-to-integer.ts
+++ roman-to-integer.ts
@@ -1,5 +1,7 @@
 function romanToInt(s: string): number {
-  const data = {
+
+
+const data = {
     I: 1,
     V: 5,
     X: 10,
@@ -10,29 +12,36 @@ function romanToInt(s: string): number {
   };
 
   const arr = [];
+
   for (let i = 0; i < s.split("").length; i++) {
-    if (s.split("")[i] === "I") {
-      if (s.split("")[i + 1] === "V" || s.split("")[i + 1] === "X") {
-        arr.push(s.split("")[i] + s.split("")[i + 1]);
-      } else {
+    switch (s.split("")[i]) {
+      case "I":
+        if (s.split("")[i + 1] === "V" || s.split("")[i + 1] === "X") {
+          arr.push(s.split("")[i] + s.split("")[i + 1]);
+        } else {
+          arr.push(s.split("")[i]);
+        }
+        break;
+      case "X":
+        if (s.split("")[i + 1] === "L" || s.split("")[i + 1] === "C") {
+          arr.push(s.split("")[i] + s.split("")[i + 1]);
+        } else {
+          arr.push(s.split("")[i]);
+        }
+        break;
+      case "C":
+        if (s.split("")[i + 1] === "D" || s.split("")[i + 1] === "M") {
+          arr.push(s.split("")[i] + s.split("")[i + 1]);
+        } else {
+          arr.push(s.split("")[i]);
+        }
+        break;
+      default:
         arr.push(s.split("")[i]);
-      }
-    } else if (s.split("")[i] === "X") {
-      if (s.split("")[i + 1] === "L" || s.split("")[i + 1] === "C") {
-        arr.push(s.split("")[i] + s.split("")[i + 1]);
-      } else {
-        arr.push(s.split("")[i]);
-      }
-    } else if (s.split("")[i] === "C") {
-      if (s.split("")[i + 1] === "D" || s.split("")[i + 1] === "M") {
-        arr.push(s.split("")[i] + s.split("")[i + 1]);
-      } else {
-        arr.push(s.split("")[i]);
-      }
-    } else {
-      arr.push(s.split("")[i]);
+        break;
     }
   }
+ 
 
   const newArr = [];
 
