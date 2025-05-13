f42db7f725668639f0a394bbeeb4b42d99e7bcae
Amirhossein Kakavand <kakavand2k@gmail.com>
2025-05-13 20:44:38 +0330

    create palindrome-number file

---
+++ src/functions/palindrome-number.ts
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/palindrome-number/description/
+
+export function isPalindrome(x: number): boolean {
+
+
+    return true
+}
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,6 @@
+import { isPalindrome } from "./functions/palindrome-number"
+
+
+isPalindrome(223)
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = ``
